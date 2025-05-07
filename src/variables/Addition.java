862608084247fe745da89c8c3626b7706f415273
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-07 17:55:22 +0530

    new classes and packages

---
+++ src/variables/Addition.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Addition {
+
+    public static void main(String[] args) {
+
+        int x=20;
+        int y=30;
+        int z=25;
+
+        int sum = x+y+z;
+        System.out.println("sum of all= "+sum);
+
+
+    }
+
+}
