a1d7a721473f91796b5af4ebc75afc222c2fbbca
Tarun1328 <tarunsachdeva1328@gmail.com>
2025-05-07 18:02:55 +0530

    new class

---
+++ src/variables/Subtraction.java
@@ -0,0 +1,18 @@
+package variables;
+
+public class Subtraction {
+    public static void main(String[] args) {
+
+        int a=200;
+        int b=170;
+        int c=5;
+
+        int sum= a-b-c;
+        System.out.println("sum of all= "+sum);
+
+
+
+    }
+
+
+}
